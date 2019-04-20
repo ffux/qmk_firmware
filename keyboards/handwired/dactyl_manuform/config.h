dc97864251348f4872945fe5526069a77d51f78a
Tim Newman <11672402+ffux@users.noreply.github.com>
2019-04-20 22:27:34 +1200

    Update config.h
    Changes to manufacturer/description text

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -24,9 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x3060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    tshort
+#define MANUFACTURER    ffux
 // defined in subfolder
-#define DESCRIPTION     A split keyboard for the cheap makers
+#define DESCRIPTION     A split keyboard for tim
 
 /* mouse config */
 #define MOUSEKEY_INTERVAL       20
@@ -38,21 +38,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
 
-/* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D0
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
 
 /* Enables This makes it easier for fast typists to use dual-function keys */
 #define PERMISSIVE_HOLD
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
-
+#define RGBLIGHT_TIMER
 #define RGBLED_NUM 12    // Number of LEDs
+
 
 /*
  * Feature disable options
